9e757966caaaed249a5d3f5745302aebe956da19
Robert Von Der Becke <vonderbeckememberships@gmail.com>
2022-05-10 13:50:18 -0500

    added redirect

--- src/components/form-popup.conponent.jsx
+++ src/components/form-popup.conponent.jsx
@@ -1,28 +1,71 @@
-import React from 'react'
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import FormInput from '../components/form-input.component';
+import { addNewUserBill } from '../utils/fireBase';
 
 const FormPopUp = () => {
-    const nav = useNavigate();
+	const [formData, setFormData] = useState({
+		company: '',
+		currentDue: 0,
+		isSettlement: false,
+		name: '',
+		pastDue: 0,
+		previousBill: 0,
+		simplePayAmount: 0,
+	});
 
+	const {
+		company,
+		currentDue,
+		isSettlement,
+		name,
+		pastDue,
+		previousBill,
+		simplePayAmount,
+	} = formData;
+	const nav = useNavigate();
 
-    const handleOnClick = () => {
+	const handleOnClick = () => {
 		nav(-1);
 	};
+	const handleFormSubmit = async (e) => {
+		e.preventDefault();
+		await addNewUserBill(formData);
+	};
 
-    return (
-      <>
-              <h1>Add new bill</h1>
-              <button
-					className='signup-container-x-btn'
-					type='button'
-					onClick={handleOnClick}>
-					X
-              </button>
-      </>
-              
-
-     
-  )
-}
+	return (
+		<div className='form-popup-container'>
+			<h1>Add new bill</h1>
+			<button
+				className='signup-container-x-btn'
+				type='button'
+				onClick={handleOnClick}>
+				X
+			</button>
+			<form onClick={handleFormSubmit}>
+				<FormInput
+					label='Company Name'
+					type='text'
+					required
+					name='company'
+					value={company}
+				/>
+				<FormInput
+					label='Current Bill Amount'
+					type='text'
+					required
+					name='currentDue'
+					value={currentDue}
+				/>
+				<FormInput
+					label='Bill Nickname'
+					type='text'
+					name='name'
+					value={name}
+				/>
+			</form>
+		</div>
+	);
+};
 
 export default FormPopUp;
--- src/routes/dashboard/dashboard.component.jsx
+++ src/routes/dashboard/dashboard.component.jsx
@@ -1,6 +1,5 @@
-import { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { BillContext } from '../../context/bills.context';
-import { addNewUserBill } from '../../utils/fireBase';
 import BillCard from '../../components/bill-card.component';
 import Bill from '../../components/bill.component';
 import FormPopUp from '../../components/form-popup.conponent';
@@ -9,24 +8,10 @@ import { Outlet } from 'react-router-dom';
 const Dashboard = () => {
 	const { bills, setBills } = useContext(BillContext);
 	const [ popUp, setPopUp ] = useState(false);
-	const [formData, setFormData] = useState({
-		company: '',
-		currentDue: 0,
-		isSettlement: false,
-		name: '',
-		pastDue: 0,
-		previousBill: 0,
-		simplePayAmount: 0,
-	});
 
-	const handleFormSubmit = async (e) => {
-		e.preventDefault();
-		await addNewUserBill(formData);
-	};
 	const handlePopUp = async () => {
 		setPopUp(true);
 	};
-	useLayoutEffect(() => {}, [setFormData]);
 
 	return (
 		<>
@@ -43,9 +28,7 @@ const Dashboard = () => {
 			{popUp && (
 				<div className='screenCover'>
 					<div className='signup-container'>
-						<form onClick={handleFormSubmit}>
 							<FormPopUp />
-						</form>
 					</div>
 				</div>
 			)}
